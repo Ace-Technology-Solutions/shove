80cf9f315d6befd780581de333d7c4f4fbe5b230
raymo <raymo@YOGA3>
2019-12-23 21:40:51 -0800

    Major change might break
    Traced root of import problems to weird code. Put in some possible fix code

--- shove/__init__.py
+++ shove/__init__.py
@@ -4,7 +4,8 @@ Common object storage frontend.
 '''
 
 from shove.core import Shove
-
+from shove.base import *
+from shove.cache import *
 __version__ = (0, 5, 4) # Forked VERSION
 
 __all__ = ['Shove']
