0a47e8ec59ae4600fb2b9f144c2de1689be54ee1
raymo <raymo@YOGA3>
2020-01-06 22:17:50 -0800

    __all__ fix

--- shove/__init__.py
+++ shove/__init__.py
@@ -6,6 +6,8 @@ Common object storage frontend.
 from shove.core import Shove
 from shove.base import *
 from shove.cache import *
+import shove.base as shoveBase
+import shove.cache as shoveCache
 __version__ = (0, 5, 4) # Forked VERSION
 
-__all__ = ['Shove','BaseStore','MemoryStore','DBMStore','ClientStore','SyncStore','FileStore']
+__all__ = ['Shove','BaseStore','MemoryStore','DBMStore','ClientStore','SyncStore','FileStore'] + shoveBase.__all__
