0c038b9c54695fcc7f46ac83fbbb45a5c60d16ee
raymo <raymo@YOGA3>
2020-01-06 22:20:25 -0800

    More bugfixing with __all__

--- shove/__init__.py
+++ shove/__init__.py
@@ -8,6 +8,7 @@ from shove.base import *
 from shove.cache import *
 import shove.base as shoveBase
 import shove.cache as shoveCache
+import shove.store as shoveStore
 __version__ = (0, 5, 4) # Forked VERSION
 
-__all__ = ['Shove','BaseStore','MemoryStore','DBMStore','ClientStore','SyncStore','FileStore'] + shoveBase.__all__
+__all__ = ['Shove','BaseStore','MemoryStore','DBMStore','ClientStore','SyncStore','FileStore'] + shoveBase.__all__ + shoveStore.__all__
